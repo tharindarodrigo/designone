d9c2f9848ffa0feef5ebec5afe929ace2997cb5c
tharindarodrigo <tharindarodrigo@gmail.com>
2016-07-29 17:51:31 +0530

    minor problems fixed

--- resources/views/control-panel/project-types/_partials/list.blade.php
+++ resources/views/control-panel/project-types/_partials/list.blade.php
@@ -1,37 +1,39 @@
-<table class="table table-bordered">
-    <thead>
-    <tr>
-        <th>#</th>
-        <th>Project Type</th>
-        <th>Controls</th>
-    </tr>
-    </thead>
-    <tbody>
-    @if(!empty($projectTypes))
-        @foreach($projectTypes as $projectType)
-            <tr>
-                <td>{!! $projectType->id !!}</td>
-                <td>{!! $projectType->project_type !!}</td>
-                <td>
-                    <div class="btn-group">
-                        {!! Form::open(['route'=>['control-panel.projects.destroy', $projectType->id], 'method'=>'delete']) !!}
-                        <a class="btn btn-sm btn-primary"
-                           href="{!! route('control-panel.project-types.edit', $projectType->id) !!}">
-                            <span class="glyphicon glyphicon-edit"></span>
-                        </a>
-                        <button class="btn btn-sm btn-danger" type="submit">
-                            <span class="glyphicon glyphicon-trash delete"></span>
-                        </button>
-                        {!! Form::close() !!}
-                    </div>
-                </td>
-            </tr>
+<div class="table-responsive">
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Project Type</th>
+            <th>Controls</th>
+        </tr>
+        </thead>
+        <tbody>
+        @if(!empty($projectTypes))
+            @foreach($projectTypes as $projectType)
+                <tr>
+                    <td>{!! $projectType->id !!}</td>
+                    <td>{!! $projectType->project_type !!}</td>
+                    <td>
+                        <div class="btn-group">
+                            {!! Form::open(['route'=>['control-panel.projects.destroy', $projectType->id], 'method'=>'delete']) !!}
+                            <a class="btn btn-sm btn-primary"
+                               href="{!! route('control-panel.project-types.edit', $projectType->id) !!}">
+                                <span class="glyphicon glyphicon-edit"></span>
+                            </a>
+                            <button class="btn btn-sm btn-danger" type="submit">
+                                <span class="glyphicon glyphicon-trash delete"></span>
+                            </button>
+                            {!! Form::close() !!}
+                        </div>
+                    </td>
+                </tr>
 
-        @endforeach
-    @endif
-    </tbody>
-</table>
-
+            @endforeach
+        @endif
+        </tbody>
+    </table>
+</div>
 @section('scripts')
 
 @endsection
+
