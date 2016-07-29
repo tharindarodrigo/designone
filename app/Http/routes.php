cfffb58eeda459df8ebc8c3d1e6e1640a8fc127e
tharindarodrigo <tharindarodrigo@gmail.com>
2016-07-29 18:50:37 +0530

    angular list created

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,9 +20,31 @@ Route::get('/', function () {
     return view('index', compact('sliderImages', 'generalContent'));
 });
 
+Route::get('/projects', function () {
+    $projects = \App\Project::with('projectTypes')->get();
+    $angularList = [];
+    foreach ($projects as $project) {
+        $projectTypeClasses = [];
+        foreach ($project->projectTypes as $projectType) {
+            $projectTypeClasses[] = snake_case($projectType->project_type);
+        }
+
+        $projectTypeClasses = implode(" ", $projectTypeClasses);
 
 
-Route::group(['prefix'=>'control-panel'], function(){
+        $angularList[] = [
+            'headingMain' => $project->project_name ? $project->project_name: 'Heading'.$project->id,
+            'thumb_image' => 'control-panel/images/projects/' . $project->id . '.jpg',
+            'large_image' => 'control-panel/images/projects/' . $project->id . '.jpg',
+            'category' => $projectTypeClasses
+        ];
+
+    }
+
+    return $angularList;
+});
+
+Route::group(['prefix' => 'control-panel'], function () {
 
     Route::get('/test', function () {
         return view('control-panel.test');
