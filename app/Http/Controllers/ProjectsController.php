7e66a10a1b7b0d3b3b64933fc3c99ad5d9c358a5
Tharinda Rodrigo <tharindarodrigo@gmail.com>
2019-01-21 13:32:45 +0530

    Image upload auto resize

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -44,9 +44,30 @@ class ProjectsController extends Controller
             $project->projectTypes()->attach($project->project_type);
 
             if ($image = Input::file('project_photo')) {
-                Image::make($image)
-                    ->encode('jpg')
-                    ->save('control-panel/images/projects/' . $project->id . '.jpg');
+
+
+                $x = Image::make($image);
+                $width = $x->width();
+                $length = $x->length();
+
+                $x->encode('jpg');
+
+                if ($width > $length) {
+                    if ($width > 1280) {
+                        $x = $x->resize(null, 1280, function ($constraint) {
+                            $constraint->aspectRatio();
+                        });
+                    }
+                } else {
+                    if ($length > 1280) {
+                        $x = $x->resize(1280, null, function ($constraint) {
+                            $constraint->aspectRatio();
+                        });
+                    }
+                }
+
+
+                $x->save('control-panel/images/projects/' . $project->id . '.jpg');
 
                 Image::make($image)
                     ->encode('jpg')
