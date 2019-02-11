b08158baaea60c9ac9e7832fea197af471b01b7d
Tharinda Rodrigo <tharindarodrigo@gmail.com>
2019-02-11 13:25:52 +0530

    Image upload auto resize

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -648,8 +648,17 @@
 <script src="{!! asset('js/map.js') !!}" type="text/javascript"></script>
 <script src="{!! asset('js/theme.js') !!}" type="text/javascript"></script>
 
-<script type="text/javascript" src="{!! asset('swiper/swiper.jquery.min.js') !!}"></script>
-<script type="text/javascript" src="{!! asset('cubeportfolio/js/jquery.cubeportfolio.min.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('vendor/jquery.min.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('vendor/jquery.migrate.min.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('vendor/bootstrap/js/bootstrap.min.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('vendor/jquery.smooth-scroll.min.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('vendor/jquery.back-to-top.min.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('vendor/scrollbar/jquery.scrollbar.min.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('vendor/swiper/swiper.jquery.min.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('vendor/cubeportfolio/js/jquery.cubeportfolio.min.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('vendor/jquery.wow.min.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('js/components/portfolio-3-col.min.js') !!}"></script>
+
 <script type="text/javascript">
     /* $(document).ready(function () {
      $('#newsModal').trigger('click');
