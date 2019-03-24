96758e747e4cf8e01e39b570c353ba7232e9075e
Tharinda Rodrigo <tharindarodrigo@gmail.com>
2019-03-24 12:33:23 +0530

    old clients

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,6 +16,7 @@
     {{-- Css --}}
     <link href="{!! asset('css/style.css')!!}" rel="stylesheet" type="text/css"/>
     <link href="{!! asset('css/bootstrap.css')!!}" rel="stylesheet" type="text/css"/>
+    {{--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">--}}
     <link href="{!! asset('css/plugins/animate.css')!!}" rel="stylesheet" type="text/css"/>
     {{-- SLIDER REVOLUTION CSS SETTINGS --}}
     <link rel="stylesheet" type="text/css" href="{!! asset('rs-plugin/css/settings.css')!!}" media="screen"/>
@@ -758,6 +759,11 @@
 <script src="{!! asset('js/jquery-1.11.2.min.js') !!}" type="text/javascript"></script>
 <script src="{!! asset('js/plugins/jquery.easing.1.3.js') !!}" type="text/javascript"></script>
 <script src="{!! asset('js/plugins/bootstrap.min.js') !!}" type="text/javascript"></script>
+
+{{--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>--}}
+{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>--}}
+{{--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>--}}
+
 <script src="{!! asset('js/plugins/jquery.fitvids.js') !!}" type="text/javascript"></script>
 <script src="{!! asset('js/plugins/jquery.stellar.min.js') !!}" type="text/javascript"></script>
 <script src="{!! asset('js/plugins/isotope.pkgd.min.js') !!}" type="text/javascript"></script>
